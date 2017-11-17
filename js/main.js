ba296c04e89e6808bdad65d968ee995700a83688
Sam Ngigi <sam.t.ngigi@gmail.com>
2017-11-17 08:44:18 +0300

    want to add slip function

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+//Modal
 $('#report').on('show.bs.modal', function (event) {
   var button = $(event.relatedTarget) // Button that triggered the modal
   var recipient = button.data('whatever') // Extract info from data-* attributes
@@ -7,3 +8,15 @@ $('#report').on('show.bs.modal', function (event) {
   modal.find('.modal-title').text('New message to ' + recipient)
   modal.find('.modal-body input').val(recipient)
 })
+//Live Analytics
+$(document).ready(function() {
+   // Stuff to do as soon as the DOM is ready
+   var messageReport = document.getElementById('message-report');
+   var reportSubmit = document.getElementById('reportSubmit');
+
+   function submitClick() {
+     alert("Submit is Working");
+   }
+   $(".dashtext-1").text("12");
+
+});
